b0829b9d42eaee11a1efb9774e5bac8f38b53612
Vincent Dhennin <vincent.dhennin@viacesi.fr>
2021-06-14 23:03:02 +0200

    revert unused import/export

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import * as CONSTANTS from "./src/Constants.js";
 import Event from "./src/Event.js";
 import Policy from "./src/Policy.js";
-import * as Policies from "@nodelint/policies";
 
-export { CONSTANTS, Event, Policy, Policies };
+export { CONSTANTS, Event, Policy };
