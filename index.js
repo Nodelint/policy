b2c35ca1edfe118b0b844920722858a67ce08c77
Vincent Dhennin <vincent.dhennin@viacesi.fr>
2021-06-14 21:26:08 +0200

    chore: get things work with policies monorepo

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import * as CONSTANTS from "./src/Constants.js";
 import Event from "./src/Event.js";
 import Policy from "./src/Policy.js";
+import * as Policies from "@nodelint/policies";
 
-export { CONSTANTS, Event, Policy };
+export { CONSTANTS, Event, Policy, Policies };
